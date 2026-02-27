fdc6828a682be73a299e20db5998680e17e1a148
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-02-27 17:09:07 -0300

    Add 2-OperadoresAtribuicao.php with assignment operators

---
+++ Aul04/ReforcoGPT/2-OperadoresAtribuicao.php
@@ -0,0 +1,33 @@
+<?php
+$x = 5; // atribui 5 à variável
+?>
+
+3-OperadoresCombinados.php
+<?php
+
+/*
+Operador	Equivale a
++=	        x = x + valor
+-=	        x = x - valor
+*=	        x = x * valor
+/=	        x = x / valor
+%=	        x = x % valor
+*/
+
+
+$x = 10;
+
+$x += 5; // x = 10 + 5
+echo $x; // 15
+
+$x -= 3; // x = 15 - 3
+echo $x; // 12
+
+$x *= 2; // x = 12 * 2
+echo $x; // 24
+
+$x /= 4; // x = 24 / 4
+echo $x; // 6
+
+
+?>

207ab5d304edf1942aa90fd9dc7dfba69ade4fab
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-02-27 15:55:46 -0300

    Add PHP script for data types demonstration
    Introduced a PHP script demonstrating various data types.
    
    Aula 3 - Tema 4 - 27.02.2026

---
+++ CodEmAula/Aul04-TiposdeDados/1-TipodeDados.php
@@ -0,0 +1,31 @@
+
+
+<?php
+
+// Tipos de Dados
+
+/*
+Referencias: w3schools data types
+*/ 
+
+# As variaveis podem serde diferentes tipos de dados
+# A mesma variavel pode ter tipos diferentes que determinam o seu tipo.
+$variavel = 100;
+$variavel = "cem";
+
+# Observamos acima atributos diferentes para a mesma variável.
+
+# Apesar das variáveis serem de tipo implícito 
+# é importante conhecermos os tipos de variáveis.
+
+$inteiro = 100; # São valores inteiros sem casas decimais
+$float = 10.5; # São valores seguidos de casas decimais.
+$bool = true; #Assume valores "falso" ou "verdadeiro"
+$string = "Boa Tarde"; # Cadeia de caracteres
+$array = [1,2,3]; # Coleção de valores
+$nulo = null; # Variável com valor nulo
+$pessoa = new Pessoa(); # Objeto com proprieades e métodos
+
+echo $inteiro;
+
+?>

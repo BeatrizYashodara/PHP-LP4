9605eb01b515a455f0b009f188a41ddd99ce2242
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-02-27 16:16:37 -0300

    Add PHP script demonstrating assignment operator

---
+++ Aul05-Operadores/1-Operador_Atribuicao.php
@@ -0,0 +1,17 @@
+<?php
+
+// =====================
+# Operadores
+# São símbolos que permitem executar operações matemáticas
+# ou lógicas / comparativas.
+
+/*
+referência: https://www.w3schools.com/php/php_operators.asp 
+*/
+
+# Operador de atribuição e o símbolo = 
+
+$variavel = 100;
+echo $variavel;
+
+?>

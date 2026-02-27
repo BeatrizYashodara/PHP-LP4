4ea9493c02a7668b649df318a603eae41283d781
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-02-27 16:06:28 -0300

    Add initial PHP script for printing to screen

---
+++ Aul03/1-PrintDeTela.php
@@ -0,0 +1,33 @@
+<?php
+
+
+// comentário único
+/*
+comentário
+de
+bloco
+
+
+Comandos: echo & print
+*/
+
+
+# Outra forma de fazer um comentário em linha
+
+
+# Exemplo 1
+echo "Aulas de PHP em LP4";
+
+
+# Pulando linhas!!
+#Forma 1
+echo"<br>";
+echo("Alu.: Beatriz");
+echo"<br>";
+echo"<br>";
+
+
+# Exemplo 2 + outra forma de pular linha ("blablablabla <br> ")!
+print "Aula 03 <br>";
+print ("Instruções de comentários e saída de dados em tela <br>");
+?>

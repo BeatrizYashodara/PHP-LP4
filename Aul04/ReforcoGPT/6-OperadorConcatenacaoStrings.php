2ba38169156db81c7c6e26c14cc9ae9881bc8b96
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-02-27 17:12:07 -0300

    Add PHP files for string concatenation examples

---
+++ Aul04/ReforcoGPT/6-OperadorConcatenacaoStrings.php
@@ -0,0 +1,21 @@
+<?php
+/*
+OPERADOR DE CONCATENAÇÃO: strings
+
+*/
+
+$nome = "Beatriz";
+$sobrenome = "Barros";
+
+echo $nome . " " . $sobrenome;
+// Beatriz Barros
+?>
+
+7-OperadorConcatenacaoAtribuicao.php
+<?php
+$texto = "Olá";
+$texto .= " Beatriz";
+
+echo $texto;
+// Olá Beatriz
+?>

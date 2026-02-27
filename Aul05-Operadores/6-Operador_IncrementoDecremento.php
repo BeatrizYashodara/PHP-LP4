4b7d43837597c83809431060089caabe1b867332
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-02-27 16:42:00 -0300

    Create 6-Operador_IncrementoDecremento.php

---
+++ Aul05-Operadores/6-Operador_IncrementoDecremento.php
@@ -0,0 +1,38 @@
+<?php
+
+// Operadores de Incremento e Decremento
+
+/* Permitem adicionar ou subtrair uma unidade da variável.
+*/
+
+$y = 1;
+echo $y = 1;
+echo "<br>";
+
+#Tradicional
+$y = 1+1;
+echo $y = 1;
+echo "<br>";
+
+# Pré-Incremento
+++$y;
+echo $y;
+echo "<br><br>";
+
+# Pós-Incremento
+$y++;
+echo $y;
+echo "<br><br>";
+
+# Pós-Decremento
+$y--;
+echo $y;
+echo "<br><br>";
+
+# Pré-Decremento
+--$y;
+echo $y;
+echo "<br><br>";
+
+
+?>

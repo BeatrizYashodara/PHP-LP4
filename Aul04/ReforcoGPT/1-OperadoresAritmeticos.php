b0da2563788b5a925c0e54246daf78c10230e857
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-02-27 17:08:34 -0300

    Add PHP script for arithmetic operators examples

---
+++ Aul04/ReforcoGPT/1-OperadoresAritmeticos.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+
+1 - OPERADORES ARITMÉTICOS
+
+Operador	Significado
+
++                | soma |
+-                | subtração |
+*                | multiplicação |
+/                | divisão |
+%                | resto (módulo) |
+**               | potência |
+
+*/
+
+$a = 10;
+$b = 3;
+
+// Soma
+echo $a + $b; // 13
+
+// Subtração
+echo $a - $b; // 7
+
+// Multiplicação
+echo $a * $b; // 30
+
+// Divisão
+echo $a / $b; // 3.3333
+
+// Resto da divisão
+echo $a % $b; // 1
+
+// Potência
+echo $a ** $b; // 1000 (10^3)
+?>

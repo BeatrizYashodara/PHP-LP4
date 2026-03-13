44ad1db6e17eefa00835c647e87e640d0b512bf3
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-03-13 15:43:43 -0300

    Create 0-Sumario.php

---
+++ 03-2026/13-03-2026/Aul12-Array/Suporte_IA/0-Sumario.php
@@ -0,0 +1,23 @@
+Arrays em PHP são fundamentais porque praticamente qualquer sistema usa listas de dados: usuários, produtos, notas, textos etc.
+
+Vou estruturar a aula como um mini-curso:
+
+Conceito de arrays
+
+Criando arrays em PHP
+
+Acessando elementos
+
+Percorrendo arrays (loops)
+
+Arrays associativos
+
+Arrays multidimensionais
+
+Funções úteis para arrays
+
+Exercícios (15 com dificuldade crescente)
+
+Resumo final
+
+Sugestão de mini-projeto

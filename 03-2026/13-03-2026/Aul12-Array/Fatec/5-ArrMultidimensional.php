bbac5c5f0604b0cfaa3070c6d4cb968e3051bd82
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-03-13 16:40:40 -0300

    Create 5-ArrMultidimensional.php

---
+++ 03-2026/13-03-2026/Aul12-Array/Fatec/5-ArrMultidimensional.php
@@ -0,0 +1,40 @@
+<?php
+
+// Array Multidimensional
+
+/**
+ * São arrays numéricos ou
+ *  associativos, cujos elementos são outros arrays.
+ * 
+ * É uma estrutura multidimensional, pois possui
+ *  elementos de array dentro de um outro array.
+ */
+
+# exemplos
+
+# 1 - multidimensional numérico
+$cod_lanches_precos = [
+    [0101, 18.50],
+    [0102, 21.5],
+    [0103, 25.6]
+];
+
+# 2 - multidimensional associativo
+$fileira_nomes = [
+    ['3' => 'expelliarmus'],
+    ['5' => 'oculus reparo'],
+    ['2' => 'alohomora']
+];
+
+# 3 - multidimensional misto
+
+$alturas_alunos = [
+    ['otávio', 1.69],
+    ['wallyson', 1.70], 
+    ['gaby', 1.58],
+    ['max', 1.85],
+    ['beia', 1.58]
+];
+
+
+?>

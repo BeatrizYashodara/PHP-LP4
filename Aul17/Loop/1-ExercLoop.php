8bb6a147b9339a86f983b1973e28ece4408cf313
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-03-27 16:26:50 -0300

    Update 1-ExercLoop.php

--- Aul17/Loop/1-ExercLoop.php
+++ Aul17/Loop/1-ExercLoop.php
@@ -10,49 +10,37 @@ primos.
 */
 
   // entradas
-      $n1 = 1;
-      $n2 = 1;
-      
-      $c = 0;
       $j = 1; // divisor
-      $divisor;
       $local = 0;
       $primos = [];
-      $qt = 0;
+      $qt = 0; // qt maxima no vetor
   
   // processos
-
-      //o array só pode ter 50 valores
-for ($i = 0; $i <= 300; $i++) {
-  
-      while ($qt < 50){
-    
-    
-        // o j sempre se renova - verificação de divisão
-        for ($j = 1; $j <= $i; $j++){
-          $local = $i;
-    
-          // verificação de número primo
+      for ($i = 0; $i <= 300; $i++) {
+      // contador de vezes que "deu zero" na multiplicação -- um primo só pode ser 
+      //divisível por um e ele mesmo de forma inteira
+      $c = 0;
+      
+      // laço interno, o j sempre se renova - verificação de divisão
+      for ($j = 1; $j <= $i; $j++){
+          
+      // verificação de número primo
             if ($local % $j == 0){
               $c += 1; }
-        }
+      }
           // se sim, armazenar em array
           if ($c == 2){
-            $primos[$qt] = $local;
+            $primos[$qt] = $i;
             $qt += 1;
             } 
-      $c = 0;
-        }   
-    }
-
-// saídas
-    for ($i = 0; $i < 50; $i++){
-        echo $i . "o numero primo: " . $primos[$i];
-            }
-
-  
-
-  
     
-  
+      // parar quando chegar em 50
+      if ($qt == 50){ break;}
+      }
+
+  // saídas
+      for ($i = 0; $i < 50; $i++){
+        echo ($i+1) . "o numero primo: " . $primos[$i] . <"br">;
+      }
+
 ?>

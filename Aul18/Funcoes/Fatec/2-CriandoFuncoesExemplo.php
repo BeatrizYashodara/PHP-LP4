02cfab4d328ae38c1902a0992e0b31cbf1a5f49d
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-17 16:27:32 -0300

    Create 2-CriandoFuncoesExemplo.php

---
+++ Aul18/Funcoes/Fatec/2-CriandoFuncoesExemplo.php
@@ -0,0 +1,21 @@
+<?php
+// Funções com parametros
+
+// Exemplo
+
+//função de adição
+
+function adicionar($a, $b){
+  echo 'Variavel recebido na função a = ' . $a . '<br>';
+  echo 'Variavel recebido na função b = ' . $b . '<br>';
+
+  return "$a + $b" = . ( $a + $b);
+}
+
+// Exemplo de aplicacao
+$x = 10;
+adicionar($x, 20);
+echo '01 - final do comando adicionar ' . '<br>';
+
+
+?>

44756ea5cbb1175cd7c4170615ec5ecd00789d38
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-17 17:37:03 -0300

    Create 5-FuncaoReturn.php

---
+++ Aul18/Funcoes/Fatec/5-FuncaoReturn.php
@@ -0,0 +1,26 @@
+<?php
+// funções - return
+/*
+a declaração return provoca o fim da execução
+do codigo de uma função
+retornando o contorle de execução para
+o coodigo
+que ahcamou a funçao
+*/
+
+function apresentar_texto(){
+echo 'este texto é apresentado';
+return;
+echo 'porem este texto nao sera apresentado';
+
+}
+
+apresentar_texto();
+
+//exemplo 2 
+echo '<hr>';
+function adicionar($a,$b);
+
+
+
+?>

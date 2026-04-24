298e54b692d2b1845d17db9502cc49bc9f0673a2
Beatriz de Barros Souza <bebarrosouza77f@gmail.com>
2026-04-24 15:46:52 -0300

    Create 1-ClasseIntroducao.php

---
+++ Fatec/Aul21_Classe_Introducao/1-ClasseIntroducao.php
@@ -0,0 +1,25 @@
+<?php
+/*
+24/04/2026
+
+ CLASSES
+Introdução de Programação Orientada a Objetos em PHP
+
+Uma classe é um molde / fôrma, uma estrutura com o qual cria-se o objeto.
+
+Exemplo: Uma Classe "Animal" é uma estrutura de atributos e funções (métodos) que permitecriar diversos tipos de animais.
+*/
+
+// Definição da sintaxe de uma Classe 
+
+class AnimaisMamiferos 
+{
+  # Definir / Criar:
+  # Propriedades
+  # Métodos
+}
+
+// Definição da sintaxe de um objeto da Classe 
+$cachorro = new AnimaisMamiferos();
+
+?>
